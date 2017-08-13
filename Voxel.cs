51433e16f161550c9a30b685e4435290136ad8bc
Adam Davidson <adam@adam-davidson.com>
2017-08-13 17:02:39 +0100

    Copy Voxels core code to separate repository
    Create a separate library for voxel reading that may eventualy be nuget'able.

---
+++ Voxel.cs
@@ -0,0 +1,11 @@
+﻿namespace Voxels {
+    public struct Voxel {
+        public byte colorIndex;
+
+        public static Voxel Empty;
+
+        public override string ToString() {
+            return colorIndex.ToString();
+        }
+    }
+}
