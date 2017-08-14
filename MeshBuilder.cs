0e2229f4b64fa4c77b603dc8b0cd425814c43258
Adam Davidson <adam@adam-davidson.com>
2017-08-14 19:32:12 +0100

    Change indices (Faces) to be 32 bit for larg .vox.

--- MeshBuilder.cs
+++ MeshBuilder.cs
@@ -20,13 +20,13 @@ namespace Voxels {
         List<XYZ> vertices = new List<XYZ>();
         List<XYZ> normals = new List<XYZ>();
         List<Color> colors = new List<Color>();
-        List<ushort> faces = new List<ushort>();
+        List<int> faces = new List<int>();
         MeshSettings settings;
 
         public XYZ[] Vertices { get { return vertices.ToArray(); } }
         public XYZ[] Normals { get { return normals.ToArray(); } }
         public Color[] Colors { get { return colors.ToArray(); } }
-        public ushort[] Faces { get { return faces.ToArray(); } }
+        public int[] Faces { get { return faces.ToArray(); } }
 
         public MeshBuilder(VoxelData voxelData, MeshSettings settings) {
             this.settings = settings;
@@ -61,21 +61,21 @@ namespace Voxels {
         void RenderQuad(VoxelData voxelData, XYZ p, Color color, XYZ faceUp) {
             // Only render quad if face it isn't hidden by voxel above it
             if (voxelData[p + faceUp].colorIndex == 0) {
-                var n = (ushort)vertices.Count;
-                var i0 = (ushort)(n + 0);
-                var i1 = (ushort)(n + 1);
-                var i2 = (ushort)(n + 2);
-                var i3 = (ushort)(n + 3);
+                var n = vertices.Count;
+                var i0 = (n + 0);
+                var i1 = (n + 1);
+                var i2 = (n + 2);
+                var i3 = (n + 3);
                 normals.AddRange(new[] { faceUp, faceUp, faceUp, faceUp });
 
                 // Calculate adjacent voxels
                 var adjacent = new XYZ[] { XYZ.Zero, XYZ.Zero, XYZ.Zero, XYZ.Zero };
-                if (faceUp == XYZ.OneX) { adjacent = new[] { XYZ.OneY, XYZ.OneZ, -XYZ.OneY, -XYZ.OneZ }; }
-                if (faceUp == XYZ.OneY) { adjacent = new[] { XYZ.OneZ, XYZ.OneX, -XYZ.OneZ, -XYZ.OneX }; }
-                if (faceUp == XYZ.OneZ) { adjacent = new[] { XYZ.OneX, XYZ.OneY, -XYZ.OneX, -XYZ.OneY }; }
-                if (faceUp == -XYZ.OneX) { adjacent = new[] { XYZ.OneZ, XYZ.OneY, -XYZ.OneZ, -XYZ.OneY }; }
-                if (faceUp == -XYZ.OneY) { adjacent = new[] { XYZ.OneX, XYZ.OneZ, -XYZ.OneX, -XYZ.OneZ }; }
-                if (faceUp == -XYZ.OneZ) { adjacent = new[] { XYZ.OneY, XYZ.OneX, -XYZ.OneY, -XYZ.OneX }; }
+                if (faceUp == -XYZ.OneX) { adjacent = new[] { XYZ.OneY, XYZ.OneZ, -XYZ.OneY, -XYZ.OneZ }; }
+                if (faceUp == -XYZ.OneY) { adjacent = new[] { XYZ.OneZ, XYZ.OneX, -XYZ.OneZ, -XYZ.OneX }; }
+                if (faceUp == -XYZ.OneZ) { adjacent = new[] { XYZ.OneX, XYZ.OneY, -XYZ.OneX, -XYZ.OneY }; }
+                if (faceUp == XYZ.OneX) { adjacent = new[] { XYZ.OneZ, XYZ.OneY, -XYZ.OneZ, -XYZ.OneY }; }
+                if (faceUp == XYZ.OneY) { adjacent = new[] { XYZ.OneX, XYZ.OneZ, -XYZ.OneX, -XYZ.OneZ }; }
+                if (faceUp == XYZ.OneZ) { adjacent = new[] { XYZ.OneY, XYZ.OneX, -XYZ.OneY, -XYZ.OneX }; }
 
                 // Calculate quad vertices based on adjacent voxels
                 var v0 = p + (adjacent[0] + adjacent[1] + faceUp + XYZ.One) / 2;
@@ -84,7 +84,7 @@ namespace Voxels {
                 var v3 = p + (adjacent[3] + adjacent[0] + faceUp + XYZ.One) / 2;
                 vertices.AddRange(new[] { v0, v1, v2, v3 });
 
-                var triangles = new ushort[] { i0, i1, i2, i2, i3, i0 };
+                var triangles = new [] { i0, i1, i2, i2, i3, i0 };
                 if (settings.AmbientOcclusion) {
                     // Calculate ambient occlusion vertex colors
                     var a00 = AmbientOcclusion.CalculateAO(voxelData, p, adjacent[0], adjacent[1], faceUp);
@@ -100,7 +100,7 @@ namespace Voxels {
 
                     // Change quad -> triangle face orientation based on ambient occlusion
                     if (a00 + a11 <= a01 + a10) {
-                        triangles = new ushort[] { i1, i2, i3, i3, i0, i1 };
+                        triangles = new [] { i1, i2, i3, i3, i0, i1 };
                     }
                 }
                 else {
@@ -109,7 +109,7 @@ namespace Voxels {
 
                 switch (settings.MeshType) {
                 case MeshType.Triangles: faces.AddRange(triangles); break;
-                case MeshType.Quads: faces.AddRange(new ushort[] { i0, i1, i2, i3 }); break;
+                case MeshType.Quads: faces.AddRange(new [] { i0, i1, i2, i3 }); break;
                 }
             }
         }
