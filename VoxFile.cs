bc7652365fa982442b08f419d3c73b30af2f74dc
Adam Davidson <adam@adam-davidson.com>
2017-08-17 17:33:47 +0100

    Added support for Voxlap .VOX Files (#1)

---
+++ VoxFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Voxels {
+    /// <summary>
+    /// Support for loading either MagicVoxel or Voxlap .VOX files.
+    /// </summary>
+    public class VoxFile {
+        public static VoxelData Read(Stream stream) {
+            var voxelData = MagicaVoxel.Read(stream);
+            if (voxelData == null) {
+            stream.Seek(0, SeekOrigin.Begin);
+                voxelData = Voxlap.Read(stream);
+            }
+            return voxelData;
+        }
+    }
+}
